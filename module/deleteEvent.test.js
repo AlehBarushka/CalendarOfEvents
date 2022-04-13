5c8cd46540c0a71339b3823db3140e1cd021462d
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-04-13 22:32:05 +0300

    added deleteEvent test

---
+++ module/deleteEvent.test.js
@@ -0,0 +1,36 @@
+import { Calendar } from './Calendar';
+
+let _ = new Calendar();
+
+jest.useFakeTimers();
+
+const validEventSample = {
+	title: 'Birthday',
+	date: '2022-06-07',
+	time: '17:46:00',
+};
+const callbackSample = () => console.log('Happy Birthday');
+const nonexistId = 'id';
+
+describe('Calendar: deleteEvent', () => {
+	const getId = () => {
+		_.addEvent(validEventSample, callbackSample);
+		return _.getEvents()[0].id;
+	};
+	test('should throw an error if called without an arg', () => {
+		try {
+			_.deleteEvent();
+		} catch (error) {
+			expect(error.message).toBe('Id is required');
+		}
+	});
+
+	test('should return a string "Event not found"', () => {
+		expect(_.deleteEvent(nonexistId)).toBe('Event not found');
+	});
+
+	test('should return a string with a successful status', () => {
+		expect(_.deleteEvent(getId())).toBe('Deleted successfully!');
+	});
+});
+
