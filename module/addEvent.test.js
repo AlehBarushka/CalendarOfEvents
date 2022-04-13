e5f4d43328723f1092f889ac9d3eb467ea8b8da2
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-04-13 22:31:45 +0300

    divided and updated the addEvent, getEvents tests

---
+++ module/addEvent.test.js
@@ -0,0 +1,83 @@
+import { Calendar } from './Calendar';
+
+jest.useFakeTimers();
+
+let _ = new Calendar();
+
+const validEventSample = {
+	title: 'Birthday',
+	date: '2022-06-07',
+	time: '17:46:00',
+};
+
+const invalidEventSample = {
+	title: 'Birthday',
+	time: '17:46:00',
+};
+
+const validEventSampleWithInvalidDate = {
+	title: 'Birthday',
+	date: '20220607',
+	time: '17:4600',
+};
+
+const validEventSampleWithElapsedDate = {
+	title: 'Birthday',
+	date: '20220607',
+	time: '17:4600',
+};
+const callbackSample = () => console.log('Happy Birthday');
+
+describe('Calendar: addEvent', () => {
+	test('should be defined', () => {
+		expect(_.addEvent(callbackSample)).toBeDefined();
+		expect(_.addEvent()).toBeDefined();
+		expect(_.addEvent(validEventSample)).toBeDefined();
+	});
+
+	test('should throw an error if called without event arg', () => {
+		try {
+			_.addEvent(_, callbackSample);
+		} catch (error) {
+			expect(error.message).toEqual('Event parameter is required');
+		}
+	});
+
+	test('should throw an error if called without callback arg', () => {
+		try {
+			_.addEvent(validEventSample);
+		} catch (error) {
+			expect(error.message).toEqual('Callback parameter is required');
+		}
+	});
+
+	test('should throw an error if called with invalid event arg', () => {
+		try {
+			_.addEvent(invalidEventSample, callbackSample);
+		} catch (error) {
+			expect(error.message).toEqual('Invalid Event');
+		}
+	});
+
+	test('should throw an error if called with invalid date format', () => {
+		try {
+			_.addEvent(validEventSampleWithInvalidDate, callbackSample);
+		} catch (error) {
+			expect(error.message).toEqual('Incorrect date format');
+		}
+	});
+
+	test('should throw an error if called with an elapsed date', () => {
+		try {
+			_.addEvent(validEventSampleWithElapsedDate, callbackSample);
+		} catch (error) {
+			expect(error.message).toEqual('The event can not be in the past');
+		}
+	});
+
+	test('should return a string with a successful status', () => {
+		expect(_.addEvent(validEventSample, callbackSample)).toBe(
+			'Added successfully!'
+		);
+	});
+});
---
+++ module/getEvents.test.js
@@ -0,0 +1,33 @@
+import { Calendar } from './Calendar';
+
+let _ = new Calendar();
+
+jest.useFakeTimers();
+
+const validEventSample = {
+	title: 'Birthday',
+	date: '2022-06-07',
+	time: '17:46:00',
+};
+const callbackSample = () => console.log('Happy Birthday');
+
+beforeAll(() => {
+	_.addEvent(validEventSample, callbackSample);
+});
+
+describe('Calendar: getEvents', () => {
+	test('should be defined', () => {
+		expect(_.getEvents()).toBeDefined();
+	});
+
+	test('should has length', () => {
+		expect(_.getEvents()).toHaveLength(1);
+	});
+
+	test('should has contain array with event object', () => {
+		expect(_.getEvents()).toEqual(
+			expect.arrayContaining([expect.objectContaining(validEventSample)])
+		);
+	});
+});
+
