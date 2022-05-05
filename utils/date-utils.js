b752254a09eb08e71b2c77eff413f551cb94ed7d
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-05-05 14:20:41 +0300

    added a reminder time object validator

--- utils/date-utils.js
+++ utils/date-utils.js
@@ -1,4 +1,10 @@
 /**
+ * @typedef {Object} ReminderTime
+ * @property {Number} ReminderTime.hours - number of hours.
+ * @property {Number} ReminderTime.minutes - number of minutes.
+ */
+
+/**
  * @description The function converts the time and date into milliseconds.
  * @param {String} date - date in format '2022-04-06'.
  * @param {String} time - time in format '12:00:00'.
@@ -14,5 +20,25 @@ export const dateParser = (date, time) => {
   const parsedDate = Date.parse(eventDate);
 
   return parsedDate;
+};
+
+/**
+ * @description The function checks the validity of the reminder time object.
+ * @param {ReminderTime} reminderTime - object of the reminder time.
+ * @returns {(Boolean|Error)} returns true if the object is valid, or returns an error if otherwise.
+ * @example
+ * //returns true
+ * reminderTimeObjValidator({hours: 12})
+ * //returns true
+ * reminderTimeObjValidator({minutes: 10, hours: 1})
+ * //returns Error
+ * reminderTimeObjValidator({})
+ */
+export const reminderTimeObjValidator = (reminderTime) => {
+  if (reminderTime?.hours || reminderTime?.minutes) {
+    return true;
+  } else {
+    throw new Error('Invalid reminder time object');
+  }
 };
 
